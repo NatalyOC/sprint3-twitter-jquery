0fd8da8aa790eeb45e19421cba7bfe66f6cd7779
NatalyOC <noteroc@hotmail.com>
2017-12-21 14:44:15 -0500

    modificando app.js

--- js/app.js
+++ js/app.js
@@ -1,25 +1,25 @@
-$(document).ready(function(){
-  /*Version 0.0.2 */
-  var max_chars = 140;
-  $('#counter').html(max_chars);
+$(document).ready(function() {
+  /* Version 0.0.2 */
+  var maxChars = 140;
+  $('#counter').html(maxChars);
   $('#tweet-box').keyup(function() {
     var chars = $(this).val().length;
-    var diff = max_chars - chars;
+    var diff = maxChars - chars;
     $('#counter').html(diff);   
-    //Habilitar boton
+    // Habilitar boton
     $('#tweet-button').prop( 'disabled', false );
-  //$(this).height($(this).prop('scrollHeight'));
-    //Mientras que el alto sea menor
-   while($(this).outerHeight() < this.scrollHeight + parseFloat($(this).css("borderTopWidth")) + parseFloat($(this).css("borderBottomWidth"))) {
-      $(this).height($(this).height()+1);}; 
+    // $(this).height($(this).prop('scrollHeight'));
+    // Mientras que el alto sea menor
+    while ($(this).outerHeight() < this.scrollHeight + parseFloat($(this).css("borderTopWidth")) + parseFloat($(this).css("borderBottomWidth"))) {
+      $(this).height($(this).height() + 1);}; 
   
 
 });
   
-   $('#tweet-button').click(function(){
-    var textTwitter=$('#tweet-box').text();
-    $('<div><p></p></div>').text(textTwitter).;
-   }); 
+  $('#tweet-button').click(function() {
+    var textTwitter = $('#tweet-box').text();
+    $('<div><p></p></div>').text(textTwitter);
+  }); 
 
   
 });
