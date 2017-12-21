4303b37a299990f0c2de134c89b0457b3ce1cd50
NatalyOC <noteroc@hotmail.com>
2017-12-21 11:26:44 -0500

    implementando version 0.0.2

--- js/app.js
+++ js/app.js
@@ -8,13 +8,18 @@ $(document).ready(function(){
     $('#counter').html(diff);   
     //Habilitar boton
     $('#tweet-button').prop( 'disabled', false );
-    /* $(this).height($(this).prop('scrollHeight')); */
-    while($(this).outerHeight() < this.scrollHeight + parseFloat($(this).css("borderTopWidth")) + parseFloat($(this).css("borderBottomWidth"))) {
-      $(this).height($(this).height()+1);
-  };
-  });
+  //$(this).height($(this).prop('scrollHeight'));
+    //Mientras que el alto sea menor
+   while($(this).outerHeight() < this.scrollHeight + parseFloat($(this).css("borderTopWidth")) + parseFloat($(this).css("borderBottomWidth"))) {
+      $(this).height($(this).height()+1);}; 
   
-    
+
+});
+  
+   $('#tweet-button').click(function(){
+    var textTwitter=$('#tweet-box').text();
+    $('<div><p></p></div>').text(textTwitter).;
+   }); 
 
   
 });
